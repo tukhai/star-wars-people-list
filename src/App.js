6b0b115c71f307c8759db8bc476f0bd4ed887f11
Admin <admin@Admins-MacBook-Pro.local>
2019-07-13 23:35:50 +0800

    Initialize react app skeleton

--- src/App.js
+++ src/App.js
@@ -2,23 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// import PeopleList from './components/PeopleList.js';
+
 function App() {
   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <h2>Star Wars People List</h2>
+        <div>
+          A single page application where users can view the details 
+          about all the people in the Star Wars Universe.
+        </div>
+
+        {/* <PeopleList 
+          peopleListData = {this.state.peopleListData}
+        /> */}
+      </div>
     </div>
   );
 }
